707d6e0f5e7923ddf5d447a53821d1f27614e5f3
Austin <thisistotallyadiscordalt@gmail.com>
2024-03-21 15:49:38 -0400

    Model trajectory code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.Measure;
 
@@ -20,6 +21,7 @@ public final class Constants {
 
   public static final double kLeftDriverEncoderDistanceConversionFactor = (24/49.66) * Units.inchesToMeters(24);
   public static final double kRightDriverEncoderDistanceConversionFactor= (24/57) * Units.inchesToMeters(24);
+  public static DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(27);
 
   public static final int PIGEON_ID = 1;
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,12 +23,25 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+
+import java.util.List;
+
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.math.trajectory.constraint.TrajectoryConstraint;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -58,7 +71,37 @@ public class RobotContainer {
 
   public static RobotContainer instance;
 
-  private final TrajectoryConfig trajConfig = new TrajectoryConfig(2, 2);
+  private final SimpleMotorFeedforward trajFeedforward = 
+    new SimpleMotorFeedforward(0.25, 1);
+
+  private final DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+    trajFeedforward, Constants.kDriveKinematics, 6);
+    
+  private final TrajectoryConfig trajConfig = new TrajectoryConfig(2, 2)
+    .setKinematics(Constants.kDriveKinematics)
+    .addConstraint(autoVoltageConstraint);
+
+  
+  private final RamseteController rController = new RamseteController();
+  private Trajectory test = 
+    TrajectoryGenerator.generateTrajectory(
+      new Pose2d(0, 0, new Rotation2d(0)),
+       List.of( new Translation2d(0, 0.5) ) , new Pose2d(0, 1,
+        new Rotation2d(0)),
+         trajConfig);
+
+  private RamseteCommand testTrajectory = new RamseteCommand(
+    test,
+    m_Drive::GetPose,
+    rController, 
+    trajFeedforward,
+    Constants.kDriveKinematics, 
+    m_Drive::GetWheelSpeeds,
+    new PIDController(0.05, 0, 0),
+    new PIDController(0.05, 0, 0),
+    m_Drive::SetDriveVoltages,
+    m_Drive );
+
   // private final TrajectoryConstraint trajConstraint = 
 
   // private static final SequentialCommandGroup postPosessionCommandGroup;
@@ -124,7 +167,7 @@ public class RobotContainer {
   }
   
   public Command getAutonomousCommand() {
-    return null;
+    return testTrajectory;
   }
   
 }
